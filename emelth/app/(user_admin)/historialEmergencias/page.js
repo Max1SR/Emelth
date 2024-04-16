7b54bbdd0d495654e3c1d13b4413e722b2fc18bb
max1sr <msanchezr2107@alumno.ipn.mx>
2024-04-16 17:38:52 -0600

    Segundo intento de solucionar error

--- emelth/app/(user_admin)/historialEmergencias/page.js
+++ emelth/app/(user_admin)/historialEmergencias/page.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Layout from "@/components/components_admin/layout";
+import Galerialoca from "@/components/galerialoca";
+import Carrusel from "@/components/carrusel";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function page() {
   return (
-    <div>
-      Historial de emergencias
-    </div>
-  )
+    <Layout>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between px-16 py-14 ${inter.className} h-full bg-slate-100 text-slate-800`}
+      >
+        <div>
+          <p>Home</p>
+        </div>
+      </main>
+    </Layout>
+  );
 }
