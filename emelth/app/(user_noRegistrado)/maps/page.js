1fc4bb588eda24ec96fcbb6848d66a2981108caa
max1sr <msanchezr2107@alumno.ipn.mx>
2024-05-29 15:58:39 -0600

    Cards para visualizacion de hospitales

--- emelth/app/(user_noRegistrado)/maps/page.js
+++ emelth/app/(user_noRegistrado)/maps/page.js
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import Layout from "@/components/components_usu_no_registrado/layout";
 import "@/styles/styles.css";
 
+import PinContainer from "@/components/3d-pin.tsx";
+
 const inter = Inter({ subsets: ["latin"] });
 export default function nosotros() {
   return (
@@ -10,7 +12,7 @@ export default function nosotros() {
       <main
         className={`flex min-h-screen flex-col items-center justify-between px-16 py-14 ${inter.className} h-full  sec2 text-slate-800`}
       >
-        <div class="concept concept-five">
+        <div className="concept concept-five">
           <h1 class="word">
             <span class="char">H</span>
             <span class="char">O</span>
@@ -25,8 +27,25 @@ export default function nosotros() {
           </h1>
         </div>
 
-        <div className=" grid grid-cols-4 gap-10 mt-6 mx-auto">
-          
+        <div className=" grid grid-cols-4 gap-10 mt-6 mx-auto"></div>
+
+        <div className="h-[40rem] w-full flex items-center justify-center ">
+          <PinContainer
+            title="/ui.aceternity.com"
+            href="https://twitter.com/mannupaaji"
+          >
+            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+              <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                Aceternity UI
+              </h3>
+              <div className="text-base !m-0 !p-0 font-normal">
+                <span className="text-slate-500 ">
+                  Customizable Tailwind CSS and Framer Motion Components.
+                </span>
+              </div>
+              <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+            </div>
+          </PinContainer>
         </div>
       </main>
     </Layout>
