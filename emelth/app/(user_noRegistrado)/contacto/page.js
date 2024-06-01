74cf15e7f632bd4be6e8e5cb29692ac9ac6a842a
max1sr <msanchezr2107@alumno.ipn.mx>
2024-06-01 01:44:53 -0600

    Inicio de sesion y registro adaptados

--- emelth/app/(user_noRegistrado)/contacto/page.js
+++ emelth/app/(user_noRegistrado)/contacto/page.js
@@ -1,63 +1,77 @@
 import React from "react";
 import { Inter } from "next/font/google";
 import Layout from "@/components/components_usu_no_registrado/layout";
-import Escudo from "@/components/iconos/escudo";
+import "@/styles/styles.css";
+import BubbleBackground from "@/components/bubbleBackgrund";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function contacto() {
   return (
     <Layout>
+      <BubbleBackground />
       <main
-        className={`flex min-h-screen flex-col items-center justify-between px-16 pt-14 ${inter.className} h-full bg-slate-100 text-slate-800`}
+        className={`flex min-h-screen flex-col items-center justify-between px-16 pt-14 ${inter.className} h-full sec2 bg-slate-100 text-slate-800`}
       >
-        <div className="flex flex-row h-full ">
-          <div className="w-1/2 min-h-full bg-slate-800 text-slate-100 p-24 rounded-l-3xl flex flex-col justify-center space-y-5">
-            <p className="text-4xl font-semibold">
-              Queremos saber lo que piensas.
-            </p>
-            <p className="font-light text-sm">
-              Haznos llegar tus opiniones o preguntas mediante el siguiente
-              formulario o el correo electrónico emelth@gmail.com.
-            </p>
+        <div className="contact-container">
+          <div className="contactInfo">
+            <div>
+              <h2>Información de Contacto</h2>
+              <ul className="info">
+                <li>
+                  <span>
+                    <img src="img/location.png" />
+                  </span>
+                  <span>
+                    Mar Mediterraneo 227 Col. Popotla
+                    <br />
+                    Miguel Hidalgo, CDMX 11400
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    <img src="img/mail.png" />
+                  </span>
+                  <span>emelth@gmail.com</span>
+                </li>
+                <li>
+                  <span>
+                    <img src="img/call.png" />
+                  </span>
+                  <span>5544332211</span>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="w-1/2 min-h-full bg-slate-200 p-24 px-32 rounded-r-3xl flex flex-col justify-center">
-            <form className="space-y-8 justify-center flex flex-col ">
-              <div className="flex flex-col space-y-2">
-                <div className="form-heading">
-                  <p className="heading-input">Nombre completo</p>
-                </div>
-                <input
-                  type="email"
-                  placeholder="Escriba su correo electrónico"
-                  className="h-9 rounded-md px-3"
-                />
+          <div className="contactForm" id="contactForm">
+            <h2>
+              Tienes dudas o problemas sobre el software? Envía un mensaje...
+            </h2>
+            <div className="formBox">
+              <div className="inputBox w50">
+                <input type="text" required />
+                <span>Nombre</span>
               </div>
-              <div className="flex flex-col space-y-2">
-                <div className="form-heading">
-                  <p className="heading-input">Correo electrónico</p>
-                </div>
-                <input
-                  type="password"
-                  placeholder="Escriba su contraseña"
-                  className="h-9 rounded-md px-3"
-                />
+              <div className="inputBox w50">
+                <input type="text" required />
+                <span>Apellido</span>
               </div>
-
-              <div className="flex flex-col space-y-2">
-                <div className="form-heading">
-                  <p className="heading-input">Mensaje</p>
-                </div>
-                <textarea
-                  type="text"
-                  placeholder="Escriba su mensaje"
-                  className=" rounded-md p-3 h-40 resize-none"
-                />
+              <div className="inputBox w50">
+                <input type="text" required />
+                <span>Correo Electronico</span>
               </div>
-              <button className="w-1/2 h-9 bg-green-600 rounded-md mx-auto">
-                Enviar
-              </button>
-            </form>
+              <div className="inputBox w50">
+                <input type="text" required />
+                <span>Telefono</span>
+              </div>
+              <div className="inputBox w100">
+                <textarea required></textarea>
+                <span>Escribe tu mensaje aqui...</span>
+              </div>
+              <div className="inputBox w100">
+                <input type="submit" value="Enviar" />
+              </div>
+            </div>
           </div>
         </div>
       </main>
--- emelth/app/(user_noRegistrado)/nosotros/page.js
+++ emelth/app/(user_noRegistrado)/nosotros/page.js
@@ -16,30 +16,135 @@ export default function Nosotros() {
       <main
         className={`flex min-h-screen flex-col items-center px-16 py-14 ${inter.className} h-full sec2 text-slate-800`}
       >
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-          <Carousel>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOqhrNbk2U2ma00x0ZZKeGvkww8mU_X7avVCJ-_eSX1FRTg0WO"
-              alt="..."
-            />
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOqhrNbk2U2ma00x0ZZKeGvkww8mU_X7avVCJ-_eSX1FRTg0WO"
-              alt="..."
-            />
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOqhrNbk2U2ma00x0ZZKeGvkww8mU_X7avVCJ-_eSX1FRTg0WO"
-              alt="..."
-            />
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOqhrNbk2U2ma00x0ZZKeGvkww8mU_X7avVCJ-_eSX1FRTg0WO"
-              alt="..."
-            />
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOqhrNbk2U2ma00x0ZZKeGvkww8mU_X7avVCJ-_eSX1FRTg0WO"
-              alt="..."
-            />
-          </Carousel>
+        <div class="img-slider">
+        <div class="slide activo">
+          <img src="1.jpg" alt="" />
+          <div class="info">
+            <h2>Nuestra Misión</h2>
+            <p>
+              "Salvar vidas a través de la innovación."<br />Nuestra misión es
+              revolucionar la manera en que los servicios de emergencia
+              responden a las crisis. Desarrollamos soluciones tecnológicas que
+              aceleran la comunicación y coordinación entre paramédicos y
+              hospitales, asegurando que cada paciente reciba la atención rápida
+              y especializada que necesita en momentos críticos.
+            </p>
+          </div>
+          <dotlottie-player
+            class="animationdot1"
+            src="https://lottie.host/d200cbba-5dab-4256-ba61-ed7eb8422af7/rb5JaJMaXL.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></dotlottie-player>
         </div>
+        <div class="slide">
+          <img src="2.jpg" alt="" />
+          <div class="info">
+            <h2>Nuestra Visión</h2>
+            <p>
+              En Emelth, aspiramos a ser pioneros en la integración de
+              soluciones tecnológicas en el campo de la atención médica de
+              emergencia, no solo en nuestras comunidades locales de Gustavo A.
+              Madero y Venustiano Carranza, sino a nivel nacional. Visualizamos
+              un mundo en el que cada llamada de emergencia se responde con la
+              velocidad y precisión que la tecnología avanzada proporciona.
+            </p>
+          </div>
+          <dotlottie-player
+            class="animationdot2"
+            src="https://lottie.host/1c64c34b-1eeb-450f-b811-c513c67bd0ba/kVK6bkmuHA.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></dotlottie-player>
+        </div>
+        <div class="slide">
+          <img src="3.jpg" alt="" />
+          <div class="info">
+            <h2>Innovación que cura</h2>
+            <p>
+              En Emelth, creemos que la tecnología tiene el poder de transformar
+              radicalmente la atención en emergencias médicas. Nuestra empresa
+              fue fundada con la visión de salvar vidas mediante el uso de
+              soluciones tecnológicas avanzadas que optimizan y agilizan la
+              comunicación entre los equipos de emergencia y las instituciones
+              hospitalarias.
+            </p>
+          </div>
+          <dotlottie-player
+            class="animationdot3"
+            src="https://lottie.host/e8f7dc27-e068-4419-bb27-8eb9eef463c4/7MN2AfzPkj.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></dotlottie-player>
+        </div>
+        <div class="slide">
+          <img src="4.jpg" alt="" />
+          <div class="info">
+            <h2>Ubicando la ayuda</h2>
+            <p class="responsive-text1">
+              Utilizamos las últimas innovaciones en geolocalización para
+              desarrollar un software que no solo responde eficazmente en
+              momentos críticos, sino que eleva la calidad del cuidado médico al
+              conectar de manera más efectiva a paramédicos y hospitales.
+              Nuestra plataforma asegura que cada paciente reciba la atención
+              adecuada rápidamente, en el lugar correcto y en el momento
+              preciso.
+            </p>
+          </div>
+          <dotlottie-player
+            class="animationdot4"
+            src="https://lottie.host/43409ce2-23e6-443f-b449-99e86304d804/L9FfxkhoW3.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></dotlottie-player>
+          <dotlottie-player
+            class="animationdot5"
+            src="https://lottie.host/a1053985-c1d4-4121-9e19-1f74b363dc3b/Rcsk2J4xpq.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></dotlottie-player>
+        </div>
+        <div class="slide">
+          <img src="5.jpg" alt="" />
+          <div class="info">
+            <h2>Mirando hacia el Futuro</h2>
+            <p class="responsive-text2">
+              Con cada actualización de nuestro software y cada nuevo servicio
+              que implementamos, nos esforzamos por estar a la vanguardia de la
+              tecnología médica de emergencia. Mientras expandimos nuestra
+              cobertura y capacidad, nuestro objetivo sigue siendo claro:
+              innovar para mejorar la vida de las personas en sus momentos más
+              vulnerables. En Emelth buscamos crear un futuro donde la atención
+              médica de emergencia sea más rápida, segura y eficaz.
+            </p>
+            <dotlottie-player
+              class="animationdot6"
+              src="https://lottie.host/225e3f4f-ed5a-420a-883c-4304397c60f2/CU5Qji6Sbz.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            ></dotlottie-player>
+          </div>
+        </div>
+        <div class="navigation">
+          <div class="btn-slide activo"></div>
+          <div class="btn-slide"></div>
+          <div class="btn-slide"></div>
+          <div class="btn-slide"></div>
+          <div class="btn-slide"></div>
+        </div>
+      </div>
       </main>
     </Layout>
   );
